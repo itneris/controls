c8de5b6add46dbdaf90fd9d017c2572358ad915c
jediar <jediar@yandex.ru>
2021-06-02 08:57:41 +0300

    Разнес контролы по разным файлам

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import CustomForm from "./CustomForm";
+import CustomControl from "./CustomControl";
+import Label from "./FormLabel";
+
+export default CustomForm;
+export const Control = CustomControl;
+export const FormLabel = Label;
